4d3ce3ecadb6aa3a26c430617d524e457c93ae8b
John Lee <john.lee@deliveryhero.com>
2022-02-09 11:23:58 +0100

    serachbox done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 // import { createClient } from 'pexel'
 
 // const httpGet = (theUrl) => {
@@ -20,8 +21,11 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
+
+
   }
 
   componentDidMount(){
@@ -30,11 +34,23 @@ class App extends React.Component {
     .then(users => this.setState({monsters:users}))
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
 
   render () {
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+      <h1> Philosophight </h1>
+        <SearchBox
+          placeholder='search monsters' 
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
